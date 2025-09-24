c3cf0d7d66a2dfb42c65d86f920e44033f0bca96
RyanDeguia <ryandeguia.dev@gmail.com>
2025-09-25 00:12:38 +0800

    finish ui of centered circle btn

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,15 @@
+import {
+  Ubuntu_400Regular,
+  Ubuntu_500Medium,
+  Ubuntu_700Bold,
+  useFonts,
+} from "@expo-google-fonts/ubuntu";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
-import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import "react-native-reanimated";
 import "../global.css";
-
-import { useColorScheme } from "@/components/useColorScheme";
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -23,31 +26,26 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    Ubuntu_400Regular,
+    Ubuntu_500Medium,
+    Ubuntu_700Bold,
     ...FontAwesome.font,
   });
 
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
   useEffect(() => {
     if (error) throw error;
   }, [error]);
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
 
   if (!loaded) {
     return null;
   }
 
+  SplashScreen.hideAsync();
+
   return <RootLayoutNav />;
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
-
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
--- app/modal.tsx
+++ app/modal.tsx
@@ -1,31 +1,44 @@
-import { StatusBar } from "expo-status-bar";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import ModalComp from "@/components/modalComponents";
+import { Ionicons } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
+import { Modal, Pressable, Text, View } from "react-native";
 
-export default function ModalScreen() {
+type props = {
+  status: boolean;
+  onpress: () => void;
+};
+export default function ModalScreen({ status, onpress }: props) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Modal</Text>
-      <View style={styles.separator} />
+    <Modal animationType="slide" transparent={true} visible={status}>
+      <Pressable
+        onPress={onpress}
+        className="flex-1 justify-center items-center bg-black/50"
+      >
+        <Pressable
+          onPress={(e) => e.stopPropagation}
+          className="w-72 rounded-xl overflow-hidden"
+        >
+          <LinearGradient
+            start={{ x: 0.5, y: 1 }}
+            end={{ x: 0.5, y: 0 }}
+            colors={["#34D399", "#F9FAFB"]}
+            className="p-6 items-center"
+          >
+            <View>
+              <Text className=" font-semibold">Smart Assistant</Text>
+              <Pressable className=" flex-row " onPress={onpress}>
+                <Ionicons
+                  size={22}
+                  className=" absolute top-[-2rem] p-1 left-[10.5rem] "
+                  name="chevron-down-outline"
+                />
+              </Pressable>
+            </View>
 
-      {/* Use a light status bar on iOS to account for the black space above the modal */}
-      <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
-    </View>
+            <ModalComp />
+          </LinearGradient>
+        </Pressable>
+      </Pressable>
+    </Modal>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-});
